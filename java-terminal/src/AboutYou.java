28925a2ead0f787e244aaf25409b9fc57e657974
Leo-Brambilla <leo.a.brambilla@gmail.com>
2023-04-05 10:58:51 -0300

    catch

--- java-terminal/src/AboutYou.java
+++ java-terminal/src/AboutYou.java
@@ -1,25 +1,32 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class AboutYou {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        try {
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Digite seu nome: ");
-        String nome = scanner.next();
+            System.out.println("Digite seu nome: ");
+            String nome = scanner.next();
 
-        System.out.println("Digite seu sobrenome: ");
-        String sobrenome = scanner.next();
+            System.out.println("Digite seu sobrenome: ");
+            String sobrenome = scanner.next();
 
-        System.out.println("Digite sua idade: ");
-        int idade = scanner.nextInt();
+            System.out.println("Digite sua idade: ");
+            int idade = scanner.nextInt();
 
-        System.out.println("Digite sua altura: ");
-        double altura = scanner.nextDouble();
+            System.out.println("Digite sua altura: ");
+            double altura = scanner.nextDouble();
 
-        System.out.println("Olá, meu nome é: "+nome+" "+sobrenome);
-        System.out.println("Tenho "+idade+" anos de idade");
-        System.out.println("Tenho "+altura+"cm de altura");
+            System.out.println("Olá, meu nome é: " + nome + " " + sobrenome);
+            System.out.println("Tenho " + idade + " anos de idade");
+            System.out.println("Tenho " + altura + "cm de altura");
+        } catch (InputMismatchException e) {
+            System.err.println("O campo idade e altura precisam ser numéricos");
+        }
+
     }
-    
+
 }
---
+++ java-terminal/src/Problem.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Problem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A, B, PROD;
+
+        
+        A = sc.nextInt();
+        System.out.println("Digite um número: ");
+        B = sc.nextInt();
+        PROD = A*B;
+        
+        
+
+        System.out.println("PROD = "+PROD);
+    }
+    
+}
